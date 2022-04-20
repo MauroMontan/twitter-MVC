566b3099ae70eb224658dd7853618dbc71d4dce0
MauroMontan <mauroanastacio@outlook.es>
2022-04-19 19:33:16 -0500

    sprint 2 finished

--- app/services/user_service.js
+++ app/services/user_service.js
@@ -4,6 +4,20 @@ class UserService {
   static create(id, username, name) {
     return new User(id, username, name, "");
   }
+
+  static getInfo(user) {
+    return [user.id, user.username, user.name];
+  }
+
+  static updateUsername(user, payload) {
+    user.username = payload;
+  }
+
+  static getAllUsernames(userList) {
+    return userList.map((user) => {
+      return user.username;
+    });
+  }
 }
 
 export default UserService;
--- test/services/user_service.test.js
+++ test/services/user_service.test.js
@@ -6,4 +6,26 @@ describe("Test for User service", () => {
 
     expect(user.username).toBe("MauroMontan");
   });
+
+  test("2) Get all users data in a list", () => {
+    const user = UserService.create(2, "MauroMontan", "Mauro");
+    const userInfoList = UserService.getInfo(user);
+    expect(userInfoList[0]).toBe(2);
+  });
+
+  test("3) Test for updateUsername method", () => {
+    const user = UserService.create(2, "MauroMontan", "Mauro");
+    UserService.updateUsername(user, "Miguel");
+
+    expect(user.username).toBe("Miguel");
+  });
+
+  test("4) Test for getAllUsernames", () => {
+    const userList = [];
+    const user = UserService.create(2, "MauroMontan", "Mauro");
+    userList.push(user);
+    const usernames = UserService.getAllUsernames(userList);
+
+    expect(usernames).toEqual(["MauroMontan"]);
+  });
 });
